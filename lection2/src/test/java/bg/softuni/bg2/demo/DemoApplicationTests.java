4de6b503fd3b7103f295b3e4a33ef362922f273b
georgibzhnv <gbozhinov@tu-sofia.bg>
2025-02-22 14:49:04 +0200

    Initial commit

---
+++ lection2/src/test/java/bg/softuni/bg2/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.bg2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
