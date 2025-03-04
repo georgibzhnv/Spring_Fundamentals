426a76220cf933803c0d65afe07428e56501b089
georgibzhnv <gbozhinov@tu-sofia.bg>
2025-03-04 18:46:33 +0200

    Initial commit.

---
+++ Lection6/src/test/java/com/softuni/lection6/Lection6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.lection6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lection6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
